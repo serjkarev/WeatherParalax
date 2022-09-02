fb6500a01b1b785fe67da8510cadebb291ff8d6c
skarev <sevenkss95@gmail.com>
2022-09-02 22:23:34 +0300

    add map anpshot support

--- WeatherParalax/WeatherModule/Presenter/WeatherPresenter.swift
+++ WeatherParalax/WeatherModule/Presenter/WeatherPresenter.swift
@@ -6,20 +6,25 @@
 //
 
 import UIKit
+import MapKit
 
 protocol WeatherViewPresenterProtocol {
     init(view: WeatherViewProtocol, with city: City?)
     var cityName: String? { get }
+    var cityImage: UIImage? { get }
 }
 
 final class WeatherPresenter {
     private weak var view: WeatherViewProtocol?
     private var city: City?
+    private var weather: Weather?
+    private var mapSnapShot: UIImage?
 
     required init(view: WeatherViewProtocol, with city: City?) {
         self.view = view
         self.city = city
         getWeather()
+        getMapSnapshot()
     }
 }
 
@@ -28,11 +33,56 @@ private extension WeatherPresenter {
     func getWeather() {
         
     }
+    
+    func getMapSnapshot() {
+        guard let lat = city?.coord.lat, let lon = city?.coord.lon else { return }
+        let options = MKMapSnapshotter.Options()
+        let center = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+        let annotation = LocationAnnotation(coordinate: center)
+        options.region = MKCoordinateRegion(center: center, span: MKCoordinateSpan())
+        options.size = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 3)//
+        options.scale = UIScreen.main.scale
+        options.mapType = .standard
+        
+        let snapshot = MKMapSnapshotter(options: options)
+        snapshot.start(with: .global()) { [weak self] (snapshot, error) in
+            guard let self = self else { return }
+            guard error == nil, let snapshot = snapshot else { return }
+            
+            DispatchQueue.main.async {
+                UIGraphicsBeginImageContextWithOptions(snapshot.image.size, true, snapshot.image.scale)
+                snapshot.image.draw(at: .zero)
+                
+                let point = snapshot.point(for: center)
+                
+                let annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "ReuseID")
+                annotationView.contentMode = .scaleAspectFit
+                annotationView.bounds = CGRect(x: 0, y: 0, width: 40, height: 40)
+                annotationView.drawHierarchy(in: CGRect(x: point.x, y: point.y, width: annotationView.bounds.size.width, height: annotationView.bounds.size.height), afterScreenUpdates: true)
+                
+                let drawnImage = UIGraphicsGetImageFromCurrentImageContext()
+                self.mapSnapShot = drawnImage
+                self.view?.succesMapDownload()
+            }
+        }
+    }
 }
 
 // MARK: - WeatherViewPresenterProtocol methods
 extension WeatherPresenter: WeatherViewPresenterProtocol {
     var cityName: String? {
         city?.name
+    }
+    
+    var cityImage: UIImage? {
+        mapSnapShot
+    }
+}
+
+final class LocationAnnotation: NSObject, MKAnnotation {
+    var coordinate: CLLocationCoordinate2D
+    
+    init(coordinate: CLLocationCoordinate2D) {
+        self.coordinate = coordinate
     }
 }
--- WeatherParalax/WeatherModule/View/WeatherViewController.swift
+++ WeatherParalax/WeatherModule/View/WeatherViewController.swift
@@ -6,30 +6,91 @@
 //
 
 import UIKit
-import MapKit
 
 protocol WeatherViewProtocol: AnyObject {
-    func succes()
+    func succesMapDownload()
     func failure(title: String, description: String)
 }
 
 final class WeatherViewController: UIViewController {
-
-    // MARK: - IBOutlet
-//    @IBOutlet private weak var mapView: MKMapView!
-    @IBOutlet private weak var currentTempLabel: UILabel!
-    @IBOutlet private weak var weatherDescriptionLabel: UILabel!
-    @IBOutlet private weak var minTempLabel: UILabel!
-    @IBOutlet private weak var maxTempLabel: UILabel!
-    @IBOutlet private weak var airHumidityLabel: UILabel!
-    @IBOutlet private weak var windSpeedLabel: UILabel!
-
+    
     var presenter: WeatherViewPresenterProtocol?
     
+    // MARK: - Constants
+    struct Constants {
+//        static fileprivate let headerHeight: CGFloat = UIScreen.main.bounds.height / 3
+        static fileprivate let headerHeight: CGFloat = UIApplication.shared.windows[0].safeAreaLayoutGuide.layoutFrame.height / 3
+    }
+
+    // MARK: - Properties
+    private var scrollView: UIScrollView!
+    
+    private var headerContainerView: UIView!
+    private var headerImageView: UIImageView!
+    
+    private var containerView: UIView!
+    private var mainStackView: UIStackView!
+    private var currTempLabel: UILabel!
+    private var descriptionLabel: UILabel!
+    private var labelsStackView: UIStackView!
+    
+    private var paramLabelsStackView: UIStackView!
+    private var minTempLabel: UILabel!
+    private var maxTempLabel: UILabel!
+    private var airHumidityLabel: UILabel!
+    private var windSpeedLabel: UILabel!
+    
+    private var valueLabelsStackView: UIStackView!
+    private var minTempValueLabel: UILabel!
+    private var maxTempValueLabel: UILabel!
+    private var airHumidityValueLabel: UILabel!
+    private var windSpeedValueLabel: UILabel!
+    
+    private var headerTopConstraint: NSLayoutConstraint!
+    private var headerHeightConstraint: NSLayoutConstraint!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavBar()
-//        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+        createUIElements()
+        addUIElements()
+        arrangeConstraints()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+//        headerImageView.image = presenter?.cityImage
+    }
+}
+
+// MARK: - UIScrollView delegate methods
+extension WeatherViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView.contentOffset.y < 0.0 {
+            headerHeightConstraint?.constant = Constants.headerHeight - scrollView.contentOffset.y
+        } else {
+            let parallaxFactor: CGFloat = 0.25
+            let offsetY = scrollView.contentOffset.y * parallaxFactor
+            let minOffsetY: CGFloat = 8.0
+            let availableOffset = min(offsetY, minOffsetY)
+            let contentRectOffsetY = availableOffset / Constants.headerHeight
+            headerTopConstraint?.constant = view.frame.origin.y
+            headerHeightConstraint?.constant = Constants.headerHeight - scrollView.contentOffset.y
+            headerImageView.layer.contentsRect = CGRect(x: 0, y: -contentRectOffsetY, width: 1, height: 1)
+        }
+    }
+}
+
+// MARK: - WeatherViewProtocol methods
+extension WeatherViewController: WeatherViewProtocol {
+    func succesMapDownload() {
+        headerImageView.image = presenter?.cityImage
+    }
+
+    func failure(title: String, description: String) {
+        let alert = UIAlertController(title: title, message: description, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alert, animated: true, completion: nil)
     }
 }
 
@@ -38,17 +99,181 @@ private extension WeatherViewController {
     func setupNavBar() {
         self.title = presenter?.cityName
     }
-}
-
-// MARK: - WeatherViewProtocol methods
-extension WeatherViewController: WeatherViewProtocol {
-    func succes() {
-        
+    
+    func createUIElements() {
+        createScrollView()
+        createHeaderContainerView()
+        createHeaderImageView()
+        createcontainerView()
+        createStacks()
+        createLabels()
+    }
+    
+    func createScrollView() {
+        scrollView = UIScrollView()
+        scrollView.delegate = self
+        scrollView.alwaysBounceVertical = true
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.backgroundColor = .white
     }
 
-    func failure(title: String, description: String) {
-        let alert = UIAlertController(title: title, message: description, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default))
-        self.present(alert, animated: true, completion: nil)
+    func createHeaderContainerView() {
+        headerContainerView = UIView()
+        headerContainerView.clipsToBounds = true
+        headerContainerView.translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    func createHeaderImageView() {
+        headerImageView = UIImageView()
+        headerImageView.translatesAutoresizingMaskIntoConstraints = false
+        headerImageView.contentMode = .scaleAspectFill
+        headerImageView.clipsToBounds = true
+    }
+
+    func createcontainerView() {
+        containerView = UIView()
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.backgroundColor = .green
+    }
+
+    func createStacks() {
+        mainStackView = UIStackView()
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        mainStackView.axis = .vertical
+        mainStackView.distribution = .fillProportionally
+        mainStackView.spacing = 40
+        
+        labelsStackView = UIStackView()
+        labelsStackView.translatesAutoresizingMaskIntoConstraints = false
+        labelsStackView.axis = .horizontal
+        labelsStackView.distribution = .fillProportionally
+        labelsStackView.spacing = 2
+        
+        paramLabelsStackView = UIStackView()
+        paramLabelsStackView.translatesAutoresizingMaskIntoConstraints = false
+        paramLabelsStackView.axis = .vertical
+        paramLabelsStackView.spacing = 20
+        
+        valueLabelsStackView = UIStackView()
+        valueLabelsStackView.translatesAutoresizingMaskIntoConstraints = false
+        valueLabelsStackView.axis = .vertical
+        valueLabelsStackView.spacing = 20
+    }
+    
+    func createLabels() {
+        currTempLabel = UILabel()
+        currTempLabel.translatesAutoresizingMaskIntoConstraints = false
+        currTempLabel.textAlignment = .center
+        currTempLabel.font = UIFont.boldSystemFont(ofSize: 60.0)
+        currTempLabel.text = "32"
+        
+        descriptionLabel = UILabel()
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.textAlignment = .center
+        descriptionLabel.font = UIFont.boldSystemFont(ofSize: 30.0)
+        descriptionLabel.text = "description"
+        
+        minTempLabel = UILabel()
+        minTempLabel.translatesAutoresizingMaskIntoConstraints = false
+        minTempLabel.textAlignment = .left
+        minTempLabel.text = "Min temperature:"
+        
+        maxTempLabel = UILabel()
+        maxTempLabel.translatesAutoresizingMaskIntoConstraints = false
+        maxTempLabel.textAlignment = .left
+        maxTempLabel.text = "Max temperature:"
+        
+        airHumidityLabel = UILabel()
+        airHumidityLabel.translatesAutoresizingMaskIntoConstraints = false
+        airHumidityLabel.textAlignment = .left
+        airHumidityLabel.text = "Air humidity:"
+        
+        windSpeedLabel = UILabel()
+        windSpeedLabel.translatesAutoresizingMaskIntoConstraints = false
+        windSpeedLabel.textAlignment = .left
+        windSpeedLabel.text = "Wind speed:"
+        
+        minTempValueLabel = UILabel()
+        minTempValueLabel.translatesAutoresizingMaskIntoConstraints = false
+        minTempValueLabel.textAlignment = .right
+        
+        maxTempValueLabel = UILabel()
+        maxTempValueLabel.translatesAutoresizingMaskIntoConstraints = false
+        maxTempValueLabel.textAlignment = .right
+        
+        airHumidityValueLabel = UILabel()
+        airHumidityValueLabel.translatesAutoresizingMaskIntoConstraints = false
+        airHumidityValueLabel.textAlignment = .right
+        
+        windSpeedValueLabel = UILabel()
+        windSpeedValueLabel.translatesAutoresizingMaskIntoConstraints = false
+        windSpeedValueLabel.textAlignment = .right
+    }
+    
+    func addUIElements() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(headerContainerView)
+        headerContainerView.addSubview(headerImageView)
+        
+        scrollView.addSubview(containerView)
+        containerView.addSubview(mainStackView)
+        
+        mainStackView.addArrangedSubview(currTempLabel)
+        mainStackView.addArrangedSubview(descriptionLabel)
+        mainStackView.addArrangedSubview(labelsStackView)
+        labelsStackView.addArrangedSubview(paramLabelsStackView)
+        labelsStackView.addArrangedSubview(valueLabelsStackView)
+
+        paramLabelsStackView.addArrangedSubview(minTempLabel)
+        paramLabelsStackView.addArrangedSubview(maxTempLabel)
+        paramLabelsStackView.addArrangedSubview(airHumidityLabel)
+        paramLabelsStackView.addArrangedSubview(windSpeedLabel)
+
+        valueLabelsStackView.addArrangedSubview(minTempValueLabel)
+        valueLabelsStackView.addArrangedSubview(maxTempValueLabel)
+        valueLabelsStackView.addArrangedSubview(airHumidityValueLabel)
+        valueLabelsStackView.addArrangedSubview(windSpeedValueLabel)
+    }
+    
+    func arrangeConstraints() {
+        let scrollViewConstraints: [NSLayoutConstraint] = [
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ]
+        NSLayoutConstraint.activate(scrollViewConstraints)
+
+        headerTopConstraint = headerContainerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+        headerHeightConstraint = headerContainerView.heightAnchor.constraint(equalToConstant: Constants.headerHeight)
+        let headerContainerViewConstraints: [NSLayoutConstraint] = [
+            headerTopConstraint,
+            headerContainerView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            headerHeightConstraint
+        ]
+        NSLayoutConstraint.activate(headerContainerViewConstraints)
+
+        let headerImageViewConstraints: [NSLayoutConstraint] = [
+            headerImageView.topAnchor.constraint(equalTo: headerContainerView.topAnchor),
+            headerImageView.leadingAnchor.constraint(equalTo: headerContainerView.leadingAnchor),
+            headerImageView.trailingAnchor.constraint(equalTo: headerContainerView.trailingAnchor),
+            headerImageView.bottomAnchor.constraint(equalTo: headerContainerView.bottomAnchor)
+        ]
+        NSLayoutConstraint.activate(headerImageViewConstraints)
+
+        let containerViewConstraints: [NSLayoutConstraint] = [
+            containerView.topAnchor.constraint(equalTo: headerContainerView.bottomAnchor),
+            containerView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            containerView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor)
+        ]
+        NSLayoutConstraint.activate(containerViewConstraints)
+        
+        let mainStackViewConstraints: [NSLayoutConstraint] = [
+            mainStackView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 40),
+            mainStackView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 10),
+            mainStackView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -10),
+            mainStackView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -20)
+        ]
+        NSLayoutConstraint.activate(mainStackViewConstraints)
     }
 }
