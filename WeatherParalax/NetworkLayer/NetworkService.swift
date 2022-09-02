84df0f8dbfcaa9d060064ffb8798712821018cf4
skarev <sevenkss95@gmail.com>
2022-09-03 00:42:55 +0300

    file management and secure apikey

--- WeatherParalax/NetworkLayer/NetworkService.swift
+++ WeatherParalax/NetworkLayer/NetworkService.swift
@@ -12,7 +12,7 @@ protocol NetworkServiceProtocol {
 }
 
 final class NetworkSevice: NetworkServiceProtocol {
-    private let appId: String = "6be715f794d3b44a90cd8a1995626f11"// move to info.plist
+    private let appId: String = Bundle.main.infoDictionary?["API_KEY"] as? String ?? "You need API_KEY"
     
     func getWeatherFor(latitude: Double, longitude: Double, completion: @escaping (Result<Weather?, Error>) -> Void) {
         var urlComponents = URLComponents()
@@ -26,7 +26,6 @@ final class NetworkSevice: NetworkServiceProtocol {
             URLQueryItem(name: "units", value: "metric")
         ]
         guard let url = urlComponents.url else { return }
-        print(url)
         URLSession.shared.dataTask(with: url) { data, _, error in
             if let error = error {
                 completion(.failure(error))
@@ -40,5 +39,4 @@ final class NetworkSevice: NetworkServiceProtocol {
             }
         }.resume()
     }
-    
 }
